5a368a58ce252e6ba8ab9720fb45e0b7f13a6f8b
John Patterson <greengatz@yahoo.com>
2016-01-30 00:16:14 -0800

    updated movement slightly

--- Mondayne/Mondayne/Assets/PlayerController.cs
+++ Mondayne/Mondayne/Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	public float speed = 0.01f;
 	private Rigidbody2D rb2d;
+	public float interactRange = 10.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,8 +18,19 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxisRaw ("Vertical");
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		movement *= speed;
 		rb2d.MovePosition (rb2d.position + movement);
 
+
+		// check for nearby objects
+		Collider2D[] nearDest = Physics2D.OverlapCircleAll (GetComponent<Transform>().position, interactRange);
+		for(int i = 0; i < nearDest.Length; i++) {
+			if (!gameObject.Equals (nearDest [i].gameObject)) {
+				Debug.Log ("near another object");
+			} else {
+				Debug.Log ("forever alone");
+			}
+		}
 	}
 
 }
