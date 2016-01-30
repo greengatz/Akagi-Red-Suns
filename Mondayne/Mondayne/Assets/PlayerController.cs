85bd696e2490f0c2d60e06c61f417454511bca83
Scott Lam <scottlam1993@yahoo.com>
2016-01-30 00:02:41 -0800

    merge conflicts

---
+++ Mondayne/Mondayne/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 0.01f;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.MovePosition (rb2d.position + movement);
+
+	}
+
+}
