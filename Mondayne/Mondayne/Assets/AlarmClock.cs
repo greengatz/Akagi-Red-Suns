b82ddc53485516ee6d12e112aa1bc77c15d93f0a
John Patterson <greengatz@yahoo.com>
2016-01-29 23:50:08 -0800

    prepare for conflicts

---
+++ Mondayne/Mondayne/Assets/AlarmClock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmClock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void selected() {
+		Debug.Log ("clock used");
+	}
+}
