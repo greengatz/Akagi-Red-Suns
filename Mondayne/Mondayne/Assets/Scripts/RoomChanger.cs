f1078525c9f530399e7325eb4ca1058a88f2f75e
John Patterson <greengatz@yahoo.com>
2016-01-30 09:39:04 -0800

    Added Room Transitions, and a bathroom

---
+++ Mondayne/Mondayne/Assets/Scripts/RoomChanger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RoomChanger : MonoBehaviour {
+
+	public float doorRange = 1.5f;
+	public string transition = "next scene";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		Collider2D[] nearDest = Physics2D.OverlapCircleAll(GetComponent<Transform>().position, doorRange);
+		for (int i = 0; i < nearDest.Length; i++)
+		{
+			Collider2D near = nearDest [i];
+
+			if (!gameObject.Equals(near.gameObject) && near.gameObject.CompareTag("Player"))
+			{
+				//nearDest[i].gameObject.SendMessage("selected");
+				Debug.Log("changing scene to " + transition);
+				//SceneManager.GetSceneByName (transition);
+				//SceneManager.SetActiveScene (SceneManager.GetSceneByName (transition));
+				SceneManager.LoadScene (transition);
+			}
+		}
+	}
+}
