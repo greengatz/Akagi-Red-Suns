00295bfe58a944d1ac20a836723538ce78944430
GodScrub <david_avila_bhc@yahoo.com>
2016-01-30 16:00:07 -0800

    Added anims again

--- Mondayne/Mondayne/Assets/Scripts/Event.cs
+++ Mondayne/Mondayne/Assets/Scripts/Event.cs
@@ -1,59 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Event : MonoBehaviour {
+public class Event : MonoBehaviour
+{
 
-	public string descriptor = "event descriptor";
-	public int eventID;
-	public float useTime = 1f;
-	public bool hideGuy = false;
-    public AudioSource[] eventAudio = null;
+    public string descriptor = "event descriptor";
+    public int eventID;
+    public float useTime = 1f;
+    public bool hideGuy = false;
+    public AudioSource eventAudio = null;
+    public SpriteRenderer objectSprite;
 
     private bool running = false;
-	private bool completed = false;
+    private bool completed = false;
 
-	// Use this for initialization
-	public void Start () {
-		if (GlobalState.events.ContainsKey(eventID)) {
-			GlobalState.events.Remove(eventID);
-		}
+    // Use this for initialization
+    public void Start()
+    {
+        if (GlobalState.events.ContainsKey(eventID))
+        {
+            GlobalState.events.Remove(eventID);
+        }
 
-		GlobalState.events.Add (eventID, this);
+        GlobalState.events.Add(eventID, this);
 
-		if (GlobalState.current > eventID) {
-			completed = true;
-		}
+        if (GlobalState.current > eventID)
+        {
+            completed = true;
+        }
 
-        eventAudio = GetComponents<AudioSource>();
+        eventAudio = GetComponent<AudioSource>();
+        objectSprite = GetComponent<SpriteRenderer>();
     }
 
-	public void startEvent() {
-		running = true;
-	}
+    public void startEvent()
+    {
+        running = true;
+    }
 
-	public bool isRunning() {
-		return running;
-	}
+    public bool isRunning()
+    {
+        return running;
+    }
 
-	public bool isCompleted() {
-		return completed;
-	}
+    public bool isCompleted()
+    {
+        return completed;
+    }
 
-	public void finishEvent() {
-		running = false;
-		completed = true;
-	}
+    public void finishEvent()
+    {
+        running = false;
+        completed = true;
+    }
 
 
-	public void selected() {
-		//Debug.Log ("selected");
-		if (isRunning ()) {
-				Debug.Log ("selected");
-				StartCoroutine (waitSelected ());
-		}
-	}
+    public void selected()
+    {
+        //Debug.Log ("selected");
+        if (isRunning())
+        {
+            Debug.Log("selected");
+            StartCoroutine(waitSelected());
+        }
+    }
 
-	IEnumerator waitSelected() {
+    IEnumerator waitSelected()
+    {
         // get the guy, hide him if we should
 
         // get the guy, hide him if we should
@@ -64,36 +77,47 @@ public class Event : MonoBehaviour {
         // play start sound
         // wait for time amount
 
-		if (eventAudio != null && eventAudio.Length >= 2)
+        if (eventAudio != null)
         {
-         
-                eventAudio[0].Play();
-                eventAudio[1].Play();
-            
+            eventAudio.Play();
         }
 
-		GlobalState.characterActing = true;
-		GlobalState.characterVis = !hideGuy;
+        if (eventID == 2)
+        {
+            Sprite isInShower = Resources.Load("BathtubClosedCurtain", typeof(Sprite)) as Sprite;
 
-        Debug.Log ("starting wait");
-		yield return new WaitForSeconds(useTime);
-		Debug.Log ("ending wait");
+            objectSprite.sprite = isInShower;
+        }
 
-		GlobalState.characterActing = false;
-		GlobalState.characterVis = true;
-		// wait for time amount       
-		// play Loop sound
-		// kill animation or whatever
-		// stop loop sound
-		// play Loop sound
-		// kill animation or whatever
-		// stop loop sound
+        GlobalState.characterActing = true;
+        GlobalState.characterVis = !hideGuy;
 
-		// play end sound
-		// make guy visible again
-		// play end sound
-		// make guy visible again
+        Debug.Log("starting wait");
+        yield return new WaitForSeconds(useTime);
+        Debug.Log("ending wait");
 
-		finishEvent ();
-	}
+        if (eventID == 2)
+        {
+            Sprite isNotInShower = Resources.Load("BathtubOpenCurtain", typeof(Sprite)) as Sprite;
+            objectSprite.sprite = isNotInShower;
+        }
+
+
+        GlobalState.characterActing = false;
+        GlobalState.characterVis = true;
+        // wait for time amount       
+        // play Loop sound
+        // kill animation or whatever
+        // stop loop sound
+        // play Loop sound
+        // kill animation or whatever
+        // stop loop sound
+
+        // play end sound
+        // make guy visible again
+        // play end sound
+        // make guy visible again
+
+        finishEvent();
+    }
 }
