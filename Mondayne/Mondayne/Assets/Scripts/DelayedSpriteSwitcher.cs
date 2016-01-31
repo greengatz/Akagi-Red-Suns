a236d36c4c661279a764abd4a7427be32eaf843d
Scott Lam <scottlam1993@yahoo.com>
2016-01-31 10:53:18 -0800

    added smoke animation for coffee

---
+++ Mondayne/Mondayne/Assets/Scripts/DelayedSpriteSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedSpriteSwitcher : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public int eventID;
+	public float delayAmount = 0.5f;
+
+	private bool canSwitch = true;
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (canSwitch) {
+			StartCoroutine (Switch ());
+		}
+	}
+
+	IEnumerator Switch() {
+		canSwitch = false;
+		if (GlobalState.events.ContainsKey (eventID) &&
+			GlobalState.events [eventID].isCompleted ()) {
+			yield return new WaitForSeconds (0.2f);
+			sr.sprite = sr.sprite == sprite1 ? sprite2 : sprite1;
+		} else {
+			sr.sprite = null;
+		}
+		canSwitch = true;
+
+	}
+}
---
+++ Mondayne/Mondayne/Assets/Scripts/SpriteSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSwitcher : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public int eventID;
+
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GlobalState.events.ContainsKey (eventID) &&
+		    GlobalState.events [eventID].isCompleted ()) {
+			sr.sprite = sprite2;
+		} else {
+			sr.sprite = sprite1;
+		}
+	}
+
+}
