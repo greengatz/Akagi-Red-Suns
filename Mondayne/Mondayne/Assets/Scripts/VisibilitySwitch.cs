4a787056911c57019912857cbadb933d86ef3433
John Patterson <greengatz@yahoo.com>
2016-01-31 14:20:35 -0800

    Tentacle Commit

--- Mondayne/Mondayne/Assets/Scripts/VisibilitySwitch.cs
+++ Mondayne/Mondayne/Assets/Scripts/VisibilitySwitch.cs
@@ -3,19 +3,34 @@ using System.Collections;
 
 public class VisibilitySwitch : MonoBehaviour {
 
-	public SpriteRenderer sprite;
-	public BoxCollider2D collider;
+	public SpriteRenderer sprite = null;
+	public BoxCollider2D collider = null;
+	public Animator anim = null;
 	public int makeVisible = 0;
 	public int makeInvisible = 0;
 
 	// Use this for initialization
 	void Start () {
 		if (GlobalState.run < makeVisible || GlobalState.run >= makeInvisible) {
-			sprite.enabled = false;
-			this.collider.enabled = false;
+			if (this.sprite != null) {
+				this.sprite.enabled = false;
+			}
+			if (this.anim != null) {
+				this.anim.enabled = false;
+			}
+			if (this.collider != null) {
+				this.collider.enabled = false;
+			}
 		} else {
-			sprite.enabled = true;
-			this.collider.enabled = true;
+			if (this.sprite != null) {
+				this.sprite.enabled = true;
+			}
+			if (this.anim != null) {
+				this.anim.enabled = true;
+			}
+			if (this.collider != null) {
+				this.collider.enabled = true;
+			}
 		}
 	}
 	
