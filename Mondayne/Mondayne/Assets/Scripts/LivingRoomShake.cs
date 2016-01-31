27ce4e26a4961842fdfe4edcd9df6b9c04f6c316
Scott Lam <scottlam1993@yahoo.com>
2016-01-31 13:08:41 -0800

    fix kevin's merge

--- Mondayne/Mondayne/Assets/Scripts/LivingRoomShake.cs
+++ Mondayne/Mondayne/Assets/Scripts/LivingRoomShake.cs
@@ -2,15 +2,30 @@
 using System.Collections;
 
 public class LivingRoomShake : MonoBehaviour {
+	public int startIteration;
+	public int stopIteration;
+	public float duration;
+
 	private CameraShake shaker;
+	private bool shake = false;
+	private float currentTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		this.shaker = this.GetComponent<CameraShake> ();
+		if (GlobalState.run >= startIteration &&
+		    GlobalState.run < stopIteration) {
+			shake = true;
+		} else {
+			shake = false;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.shaker.Shake (2.0f);
+		if (shake && currentTime < duration) {
+			this.shaker.Shake ();
+			currentTime += Time.fixedDeltaTime;
+		}
 	}
 }
